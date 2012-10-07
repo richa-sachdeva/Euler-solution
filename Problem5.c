e4540860b56778334934d3602683610efef4b0ee
richa-sachdeva <sach.richa@gmail.com>
2012-10-07 16:22:44 +0530

    Problem5 soln file, and next higher palind file added

---
+++ Problem5.c
@@ -0,0 +1,10 @@
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[]) {
+
+
+return 0;
+
+}
---
+++ higherpalin.c
@@ -0,0 +1,10 @@
+//To find next higher Palindrome of a given number
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[]) {
+
+
+return 0;
+
+}
